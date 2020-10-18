aecff8dc0d51c6aa24b6d4233ada6bc67861b658
Pablo Lezcano <47581770+pablolezcano@users.noreply.github.com>
2020-10-18 11:55:41 -0300

    estructura

--- basededatoswp.php
+++ basededatoswp.php
@@ -31,7 +31,6 @@ function db_admin_menu_function() {
         <iframe width="900" height="800" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSpIS_pCd6rXJgN_yTMjwF1g_-v_lW8hW6WkIg-zueVmG_m4mLumY01bg7pSmjvV3Xc0C9eYqsG2GrS/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe> 
         '
         ;
-
 }
 function bd_options_function() {
     echo ' 
