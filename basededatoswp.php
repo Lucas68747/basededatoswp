83c700c4ce8d591693d55b6c6224f8bba22b8620
Pablo Lezcano <47581770+pablolezcano@users.noreply.github.com>
2020-10-18 11:52:39 -0300

    estructura

---
+++ basededatoswp.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+* Plugin Name: Base de datos
+* Plugin URI: https://bootstrat.site
+* Description: Base de datos offline
+* Version: 1.0.0
+* Author: Pablo Lezca
+* Author URI: https://bootstrat.site
+* License: GPL2
+*/
+
+
+add_action('admin_menu', 'db_admin_menu');
+
+// function db_menu (){
+//     add_menu_page('BD Custom', 'BD Custom', 'manage_options', __FILE__, 'bd_edit');
+
+// }
+
+function db_admin_menu(){
+    
+    add_menu_page ( 'BD Custom', 'BD Custom', 'manage_options', 'db_admin_menu_plugin', 'db_admin_menu_function', 'dashicons-database', 10 );
+    add_submenu_page ( 'db_admin_menu_plugin', 'BD Options', 'Options', 'manage_options', 'bd_options_submenu1' ,'bd_options_function' );
+    add_submenu_page ( 'db_admin_menu_plugin', 'BD Settings', 'Settings', 'manage_options', 'bd_options_submenu2', 'bd_settings_function');
+}
+
+
+function db_admin_menu_function() {
+        echo ' 
+        <iframe width="900" height="800" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSpIS_pCd6rXJgN_yTMjwF1g_-v_lW8hW6WkIg-zueVmG_m4mLumY01bg7pSmjvV3Xc0C9eYqsG2GrS/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe> 
+        '
+        ;
+
+}
+function bd_options_function() {
+    echo ' 
+    <p> Seccion para setear el plugin </p>
+    '
+    ;
+
+}
+function bd_settings_function() {
+    echo ' 
+    <p> Configuracion del plugin</p>    '
+    ;
+
+}
+
+?>
